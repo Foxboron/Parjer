42e71891783ed71d6bb4e1356931b28a4c7288fb
Foxboron <mcfoxax@gmail.com>
2013-03-04 00:38:51 +0100

    oops

---
+++ src/parjer/network.clj
@@ -0,0 +1,46 @@
+(ns parjer.network
+  (:use [clojure.java.io :only [reader writer]]
+        [parjer.parser :as par])
+  (:import (java.net Socket)
+           (java.io PrintWriter InputStreamReader BufferedReader)))
+
+(def nick "Cjoey")
+
+(def live? (atom false))
+
+(def server {:server "bd.ntg.cx"
+             :port 13337
+             :chan "#lobby"})
+
+(defn conn-handler [c]
+  (while (nil? (:exit @c))
+    (let [msg (.readLine (:in @c))]
+      (par/irc-parse c msg))))
+
+(defn connect [serv]
+  (let [sock (Socket. (:server server) (:port server))
+        in (BufferedReader. (InputStreamReader. (.getInputStream sock)))
+        out (PrintWriter. (.getOutputStream sock))
+        conn (ref {:in in :out out})]
+    (doto
+        (Thread.
+         #(conn-handler conn)) (.start))
+    conn))
+
+
+(defn writeToOut [c msg]
+  (doto (:out @c)
+    (.println (str msg "\r"))
+    (.flush)))
+
+(defn sendinfo [conn]
+  (writeToOut conn (str "NICK " nick))
+  (writeToOut conn (str "USER " nick " 0 * : " nick)))
+
+
+(defn join [x]
+  nil)
+
+(defn ccn []
+  (let [irc (connect server)]
+    (sendinfo irc)))
