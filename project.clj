c4fc4055f87832894ab0a6cfd0b0782c2db9c553
Morten Linderud <mcfoxax@gmail.com>
2013-04-20 01:18:20 +0200

    lein-daemon

--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                   [clojail "1.0.5"]
                   [enlive "1.0.1"]]
-  :main parjer.core)
+  :plugins [[lein-daemon "0.5.1"]]
+  :main parjer.core
+  :daemon {:parjer-daemon {:ns parjer.core
+                    :pidfile "parjer.pid"}})
