4382801fafeb50f804f2eacf4db3ffd398d5bea8
Morten Linderud <mcfoxax@gmail.com>
2013-04-20 01:23:51 +0200

    added fix

--- project.clj
+++ project.clj
@@ -8,5 +8,7 @@
                   [enlive "1.0.1"]]
   :plugins [[lein-daemon "0.5.1"]]
   :main parjer.core
-  :daemon {:parjer-daemon {:ns parjer.core
-                    :pidfile "parjer.pid"}})
+  :daemon {:parjer {:ns parjer.core
+                    :pidfile "parjer.pid"}
+           ":parjer" {:ns parjer.core
+                        :pidfile "parjer.pid"}})
